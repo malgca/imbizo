f9a74b1658c18e371b340da520245f448f378466
malgca <malgca.utility@gmail.com>
2015-04-27 11:24:02 +0200

    Just created a folder with some stuff for an Imbizo.

---
+++ example.js
@@ -0,0 +1,16 @@
+$myBox = 5;
+
+// this is what you've said
+// computer create a box for me called "myBox";
+// put the number 5 in myBox;
+// stop taking commands;
+
+// computer create another box called otherBox and put 4 in it;
+
+$otherBox = 4;
+
+$fullBox = $myBox + $otherBox;
+
+//computer, please put whatever is in fullBox on the console;
+
+console.log("fullbox is equal to: " + $fullBox);
